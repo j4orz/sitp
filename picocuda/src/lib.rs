303b96fdb4d3d30761149286c9ad64f5bb07def9
jeffzh4ng <zhang.jeffreyd@gmail.com>
2025-02-02 07:36:50 -0500

    create tensor struct

--- picocuda/src/lib.rs
+++ picocuda/src/lib.rs
@@ -1,4 +1,58 @@
 use pyo3::prelude::*;
+// pub mod nn;
+
+enum Layout {
+    Strided,
+}
+
+enum Device {
+    Cpu,
+    Cuda,
+}
+
+struct Tensor {
+    // logical
+    shape: Vec<usize>,
+    stride: Vec<usize>,
+    offset: bool,
+    grad: Box<Tensor>,
+
+    // physical
+    layout: Layout,
+    device: Device,
+    data: bool,
+    dtype: bool,
+}
+
+fn tensor(input: Vec<usize>) -> Tensor {
+    Tensor::new()
+}
+
+impl Tensor {
+    fn new() -> Self {
+        todo!()
+    }
+
+    fn randn() -> Self {
+        todo!()
+    }
+
+    fn zeros() -> Self {
+        todo!()
+    }
+
+    fn ones() -> Self {
+        todo!()
+    }
+
+    fn reshape(&self) -> Self {
+        todo!()
+    }
+
+    fn view(&self) -> Self {
+        todo!()
+    }
+}
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
