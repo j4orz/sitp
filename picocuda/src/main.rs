43a508fa9acccc6210f1b7e70914a6e5758f1aa5
jeffzh4ng <zhang.jeffreyd@gmail.com>
2025-02-02 14:45:11 -0500

    beware of dragons

--- picocuda/src/main.rs
+++ picocuda/src/main.rs
@@ -2,6 +2,19 @@ mod lib; // TODO?
 use lib::Tensor;
 
 fn main() {
+    println!(
+        "
+    ⠀⠀⠀⠀⠀⣼⣧⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⣼⣿⣿⣧⠀⠀⠀⠀
+    ⠀⠀⠀⠾⠿⠿⠿⠿⠷⠀⠀⠀
+    ⠀⠀⣼⣆⠀⠀⠀⠀⣰⣧⠀⠀
+    ⠀⣼⣿⣿⣆⠀⠀⣰⣿⣿⣧⠀
+    ⠾⠟⠿⠿⠿⠧⠼⠿⠿⠿⠻⠷
+    picograd: torch.tensor->{{PTX, Triton}} compiler
+    ⚠︎ this is a one man effort to compile gpt2, llama3, and r1. beware of dragons ⚠︎
+    "
+    );
+
     let x = Tensor::randn(&[2, 3]);
     println!("{}", x);
 }
