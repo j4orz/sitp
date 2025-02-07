b308e7023e31fb33895c79b3584750097429e48c
jeffzh4ng <zhang.jeffreyd@gmail.com>
2025-02-07 08:17:18 -0500

    implement PartialEq, Eq, and Hash for Op

--- picocuda/src/differentiator.rs
+++ picocuda/src/differentiator.rs
@@ -1,74 +1,9 @@
-// *****************************************************************************************************************
-// ******************************************** .forward()/.backward() *********************************************
-// *****************************************************************************************************************
-
 use std::{
     collections::HashSet,
     ops::{Add, Mul},
 };
 
 use crate::Tensor;
-
-// autodifferentiation is algorithmic but uses numeric objects, not symbolic.
-impl Tensor {
-    // forward mode: implicit graph: (V,E) = [f(x)=TODO, (f'(x)=path_i)edge_i]
-    // backward mode: explicit graph 1. .forward() 2. .backward()
-    // -> deep learning uses reverse mode because the function class of neural networks
-    // usually has more inputs (sources) than outputs (sinks) due to the lifting of representations/motifs/embeddings
-    // reverse mode lets us reuse the outputs and keeping the computation for the massive input fan in shallow insteaad
-    // of forwards deep computation
-    // -> forward mode for neural networks is naive recursion
-    // -> backward mode for neural networks is reuse of dependencies (dp)
-
-    // .grad is sum over product of path weights
-    //          \sigma_{path0...path1}(\prod_{pathi} ei)
-
-    // reversemode/forwardmode is same for \mathbb{R} because of associativity
-    // but with \mahtbb{R^{nxm}} you have to make sure matrices are associative
-    pub fn backward(mut self) -> () {
-        self.grad = Some(Box::new(vec![Tensor::ones(&self.shape)])); // base case dfdx
-        let (mut topo, mut visited) = (Vec::new(), HashSet::new());
-        self.topo(&mut topo, &mut visited);
-        for mut tensor in topo.into_iter().rev() {
-            if let Some(ref op) = tensor.input {
-                tensor.grad = Some(Box::new(op.backward(&tensor)));
-            }
-        }
-    }
-
-    fn topo(&self, topo: &mut Vec<Tensor>, visited: &mut HashSet<*const Op>) {
-        match self.input {
-            Some(ref input) => {
-                if visited.contains(&input) {
-                    return;
-                }
-                visited.insert(input);
-
-                // flatten inputs: tup -> vec
-                let inputs = match &**input {
-                    Op::Add(x, y)
-                    | Op::Sub(x, y)
-                    | Op::Mul(x, y)
-                    | Op::Div(x, y)
-                    | Op::Matmul(x, y) => vec![x, y],
-                    Op::Sin(x)
-                    | Op::Cos(x)
-                    | Op::Exp(x)
-                    | Op::Log(x)
-                    | Op::Tanh(x)
-                    | Op::Mean(x)
-                    | Op::Var(x) => vec![x],
-                };
-
-                for input in inputs {
-                    input.topo(topo, visited);
-                }
-                topo.push(self.clone());
-            }
-            None => todo!(),
-        }
-    }
-}
 
 #[rustfmt::skip]
 #[derive(Clone, Debug)]
@@ -81,6 +16,25 @@ pub enum Op {
     Mean(Tensor), Var(Tensor), // statistics
 }
 
+impl PartialEq for Op {
+    fn eq(&self, other: &Self) -> bool {
+        std::ptr::eq(self, other) // reference comparison instead of value since f32 is not Eq.
+    }
+}
+
+impl Eq for Op {}
+
+impl std::hash::Hash for Op {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        // Hash the memory address of self
+        (self as *const Self).hash(state);
+    }
+}
+
+// *****************************************************************************************************************
+// ******************************************** .forward() *********************************************
+// *****************************************************************************************************************
+
 impl Op {
     fn forward(&self) -> Tensor {
         match &self {
@@ -88,38 +42,6 @@ impl Op {
             Op::Sub(x, y) => self.apply_binary_op(|xi, yi| xi - yi, x, y),
             Op::Mul(x, y) => self.apply_binary_op(|xi, yi| xi * yi, x, y),
             Op::Div(x, y) => self.apply_binary_op(|xi, yi| xi / yi, x, y),
-            Op::Sin(x) => todo!(),
-            Op::Cos(x) => todo!(),
-            Op::Exp(x) => todo!(),
-            Op::Log(x) => todo!(),
-            Op::Matmul(x, y) => todo!(),
-            Op::Tanh(x) => todo!(),
-            Op::Mean(x) => todo!(),
-            Op::Var(x) => todo!(),
-        }
-    }
-
-    fn backward(&self, grad: &Tensor) -> Vec<Tensor> {
-        match self {
-            Op::Add(_x, _y) => {
-                // d/dx(x + y) = 1, d/dy(x + y) = 1
-                let (dx, dy) = (Tensor::ones(&grad.shape), Tensor::ones(&grad.shape));
-                vec![&dx * &grad.clone(), &dy * &grad.clone()]
-            }
-            Op::Sub(_x, _y) => {
-                // d/dx(x - y) = 1, d/dy(x - y) = -1
-                let (dx, dy) = (
-                    Tensor::ones(&grad.shape),
-                    Tensor::new(vec![-1.0; grad.numel()]),
-                );
-                vec![&dx * &grad.clone(), &dy * &grad.clone()]
-            }
-            Op::Mul(x, y) => {
-                // d/dx(x * y) = y, d/dy(x * y) = x
-                let (dx, dy) = (y, x);
-                vec![dx * &grad.clone(), dy * &grad.clone()]
-            }
-            Op::Div(x, y) => todo!(),
             Op::Sin(x) => todo!(),
             Op::Cos(x) => todo!(),
             Op::Exp(x) => todo!(),
@@ -206,6 +128,105 @@ impl Tensor {
 
     pub fn var(&self) -> Tensor {
         todo!()
+    }
+}
+
+// *****************************************************************************************************************
+// ******************************************** .backward() *********************************************
+// *****************************************************************************************************************
+
+// autodifferentiation is algorithmic but uses numeric objects, not symbolic.
+impl Tensor {
+    // forward mode: implicit graph: (V,E) = [f(x)=TODO, (f'(x)=path_i)edge_i]
+    // backward mode: explicit graph 1. .forward() 2. .backward()
+    // -> deep learning uses reverse mode because the function class of neural networks
+    // usually has more inputs (sources) than outputs (sinks) due to the lifting of representations/motifs/embeddings
+    // reverse mode lets us reuse the outputs and keeping the computation for the massive input fan in shallow insteaad
+    // of forwards deep computation
+    // -> forward mode for neural networks is naive recursion
+    // -> backward mode for neural networks is reuse of dependencies (dp)
+
+    // .grad is sum over product of path weights
+    //          \sigma_{path0...path1}(\prod_{pathi} ei)
+
+    // reversemode/forwardmode is same for \mathbb{R} because of associativity
+    // but with \mahtbb{R^{nxm}} you have to make sure matrices are associative
+    pub fn backward(mut self) -> () {
+        self.grad = Some(Box::new(vec![Tensor::ones(&self.shape)])); // base case dfdx
+        let (mut topo, mut visited) = (Vec::new(), HashSet::new());
+        self.topo(&mut topo, &mut visited);
+        for mut tensor in topo.into_iter().rev() {
+            if let Some(ref op) = tensor.input {
+                tensor.grad = Some(Box::new(op.backward(&tensor)));
+            }
+        }
+    }
+
+    fn topo(&self, topo: &mut Vec<Tensor>, visited: &mut HashSet<Op>) {
+        match self.input {
+            Some(ref op) => {
+                if visited.contains(&op) {
+                    return;
+                }
+                visited.insert(*op.clone()); // Box<_>?
+
+                // flatten inputs: tup -> vec
+                let inputs = match &**op {
+                    Op::Add(x, y)
+                    | Op::Sub(x, y)
+                    | Op::Mul(x, y)
+                    | Op::Div(x, y)
+                    | Op::Matmul(x, y) => vec![x, y],
+                    Op::Sin(x)
+                    | Op::Cos(x)
+                    | Op::Exp(x)
+                    | Op::Log(x)
+                    | Op::Tanh(x)
+                    | Op::Mean(x)
+                    | Op::Var(x) => vec![x],
+                };
+
+                for input in inputs {
+                    input.topo(topo, visited);
+                }
+                topo.push(self.clone());
+            }
+            None => todo!(),
+        }
+    }
+}
+
+impl Op {
+    fn backward(&self, grad: &Tensor) -> Vec<Tensor> {
+        match self {
+            Op::Add(_x, _y) => {
+                // d/dx(x + y) = 1, d/dy(x + y) = 1
+                let (dx, dy) = (Tensor::ones(&grad.shape), Tensor::ones(&grad.shape));
+                vec![&dx * &grad.clone(), &dy * &grad.clone()]
+            }
+            Op::Sub(_x, _y) => {
+                // d/dx(x - y) = 1, d/dy(x - y) = -1
+                let (dx, dy) = (
+                    Tensor::ones(&grad.shape),
+                    Tensor::new(vec![-1.0; grad.numel()]),
+                );
+                vec![&dx * &grad.clone(), &dy * &grad.clone()]
+            }
+            Op::Mul(x, y) => {
+                // d/dx(x * y) = y, d/dy(x * y) = x
+                let (dx, dy) = (y, x);
+                vec![dx * &grad.clone(), dy * &grad.clone()]
+            }
+            Op::Div(x, y) => todo!(),
+            Op::Sin(x) => todo!(),
+            Op::Cos(x) => todo!(),
+            Op::Exp(x) => todo!(),
+            Op::Log(x) => todo!(),
+            Op::Matmul(x, y) => todo!(),
+            Op::Tanh(x) => todo!(),
+            Op::Mean(x) => todo!(),
+            Op::Var(x) => todo!(),
+        }
     }
 }
 
