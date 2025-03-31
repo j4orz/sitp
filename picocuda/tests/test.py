8248d96adeac0125aae9c20b3e4f6b32120441bc
jeffzh4ng <zhang.jeffreyd@gmail.com>
2025-03-31 15:37:45 -0400

    clean up test.py

--- picocuda/tests/test.py
+++ picocuda/tests/test.py
@@ -1,8 +1,8 @@
 import unittest
+import os
 import numpy as np
 import torch
 import picograd
-import os
 
 # see: https://pytorch.org/docs/stable/notes/numerical_accuracy.html
 
@@ -63,8 +63,8 @@ class TestViewOps(unittest.TestCase):
     #   x.reshape([])
 
   def test_permute(self):
-    assrt([(4,3)], lambda x: x.permute((1,0)))
-    # TODO:
+    pass
+    # assrt([(4,3)], lambda x: x.permute((1,0)))
     # assrt([(4,3)], lambda x: x.permute((0,1)))
     # assrt([(1,2,3,4)], lambda x: x.permute((3,0,2,1)))
     # assrt([(3,4,5,6)], lambda x: x.permute((3,2,1,0)))
@@ -134,12 +134,66 @@ class TestUOps(unittest.TestCase):
 
   def test_exp(self):
     assrt([(3,3)], lambda x: torch.exp(x), lambda x: picograd.exp(x))
+    assrt([(45,65)], torch.exp, Tensor.exp)
+    assrt(None, torch.exp, Tensor.exp, vals=[[math.inf, -math.inf, math.nan]])
+    assrt([()], torch.exp, Tensor.exp)
 
   def test_log(self):
-    assrt([(3,3)], lambda x: torch.log(x), lambda x: picograd.log(x))
+    pass
 
+class TestBinOps(unittest.TestCase):
+  def test_add(self):
+    assrt([(45,68), (45,68)], lambda x,y: x+y, Tensor.add)
+    assrt([(45,68), (45,68)], lambda x,y: x+y)
+    assrt([(), ()], lambda x,y: x+y)
+    assrt([(45,65), (45,1)], lambda x,y: x+y)
+    assrt([(45,65), ()], lambda x,y: x+y)
+    assrt([(45,65), (65,)], lambda x,y: x+y)
+    assrt([(3,3)], lambda x: x+8)
+    assrt([(3,3)], lambda x: x+-1)
+
+  def test_sub(self):
+    assrt([(3,3), (3,3)], lambda x,y: x-y)
+    assrt([(3,3)], lambda x: x-8)
+    assrt([(3,3)], lambda x: x+-1)
+
+  def test_mul(self):
+    assrt([(3,3), (3,3)], lambda x,y: x*y)
+    assrt([(3,1), (1,3)], lambda x,y: x*y)
+    assrt([(3,3)], lambda x: x*8)
+    # TODO: assrt([(3,3)], lambda x: 8*x)
+    assrt([(3,3)], lambda x: x*-1)
+    assrt([(45,65)], lambda x: x*2)
+    assrt([(45,65)], lambda x: x*-1)
+    assrt([(45,65)], lambda x: 255*x)
+    assrt([(45,65)], lambda x: 2*x)
+    assrt([()], lambda x: x*2)
+    assrt([()], lambda x: 2*x)
+
+  def test_div(self):
+    assrt([(3,3), (3,3)], lambda x,y: x/y)
+    assrt([(45,65), (45,65)], lambda x,y: x/y, Tensor.div)
+    assrt([(45,65), (45,65)], lambda x,y: x/y)
+    assrt([(), ()], lambda x,y: x/y)
+    assrt([(3,3)], lambda x: x/8)
+    assrt([(3,3)], lambda x: x/-1)
+    helper_test_op([(45,65)], lambda x: x/255)
+    helper_test_op([(45,65)], lambda x: x/1)
+    helper_test_op([(45,65)], lambda x: 1/x)
+    helper_test_op([(45,65)], lambda x: x/2)
+    helper_test_op([(45,65)], lambda x: 2/x)
+    helper_test_op([()], lambda x: x/2)
+    helper_test_op([()], lambda x: 2/x)
+
+  def test_matmul(self):
+    assrt([(3), (3,10)], lambda x,y: x.matmul(y), lambda x,y: x @ y)
+    assrt([(3,10), (10)], lambda x,y: x.matmul(y), lambda x,y: x @ y)
+    assrt([(3,3), (3,3)], lambda x,y: x.matmul(y), lambda x,y: x @ y)
+
+class TestReduceOps(unittest.TestCase):
   def test_sum(self):
-    assrt([(45,3)], lambda x: x.sum())
+    # TODO:  for fnn verify picograd.sum(softmax()) == 1
+    # assrt([(45,3)], lambda x: x.sum())
     # assrt([(3,4,5,6)], lambda x: x.sum(axis=3))
     # assrt([(3,4,5,6)], lambda x: x.sum(axis=(1,3)))
     # assrt([(3,4,5,6)], lambda x: x.sum(axis=(0,2)))
@@ -153,95 +207,6 @@ class TestUOps(unittest.TestCase):
     # self.helper_test_exception([(3,4,5,6)], lambda x: x.sum(5), lambda x: x.sum(5), expected=IndexError)
     # self.helper_test_exception([()], lambda x: x.sum(1), lambda x: x.sum(1), expected=IndexError)
     # self.helper_test_exception([()], lambda x: x.sum((1,)), lambda x: x.sum((1,)), expected=IndexError)
-
-class TestBinOps(unittest.TestCase):
-  def test_add(self):
-    assrt([(3,3), (3,3)], lambda x,y: x+y)
-
-  def test_add_scalar(self):
-    assrt([(3,3)], lambda x: x+8)
-    assrt([(3,3)], lambda x: x+-1)
-
-  def test_sub(self):
-    assrt([(3,3), (3,3)], lambda x,y: x-y)
-
-  def test_sub_scalar(self):
-    assrt([(3,3)], lambda x: x-8)
-    assrt([(3,3)], lambda x: x+-1)
-
-  def test_mul(self):
-    assrt([(3,3), (3,3)], lambda x,y: x*y)
-    assrt([(3,1), (1,3)], lambda x,y: x*y)
-
-  def test_mul_scalar(self):
-    assrt([(3,3)], lambda x: x*8)
-    # assrt([(3,3)], lambda x: 8*x)
-    assrt([(3,3)], lambda x: x*-1)
-
-  def test_div(self):
-    assrt([(3,3), (3,3)], lambda x,y: x/y)
-
-  def test_div_scalar(self):
-    assrt([(3,3)], lambda x: x/8)
-    assrt([(3,3)], lambda x: x/-1)
-
-  def test_matmul(self):
-    assrt([(3), (3,10)], lambda x,y: x.matmul(y), lambda x,y: x @ y)
-    assrt([(3,10), (10)], lambda x,y: x.matmul(y), lambda x,y: x @ y)
-    assrt([(3,3), (3,3)], lambda x,y: x.matmul(y), lambda x,y: x @ y)
-
-class TestReduceOps(unittest.TestCase):
-  def test_sum(self):
-    pass
-
-  def test_prod(self):
-    pass
-
-  def test_max(self):
-    pass
-
-  def test_min(self):
-    pass
-
-  def test_mean(self):
-    pass
-
-  def test_var(self):
-    pass
-
-  def test_std(self):
-    pass
-
-  def test_softmax(self):
-    pass
-
-  def test_argmax(self):
-    pass
-
-  def test_logsumexp(self):
-    pass
-
-  def test_logcumsumexp(self):
-    pass
-
-# class TestProcessingOps(unittest.TestCase):
-#   def test_matmul(self):
-#     pass
-
-#   def test_dot(self):
-#     pass
-
-#   # def test_conv2d(self):
-#   #   pass
-
-#   # def test_avgpool2d(self):
-#   #   pass
-
-#   # def test_maxpool2d(self):
-#   #   pass
-
-# #   def test_topk(self):
-# #     pass
 
 class TestNetworkOps(unittest.TestCase):
   def test_linear(self):
@@ -262,8 +227,105 @@ class TestNetworkOps(unittest.TestCase):
   def test_nllloss(self):
     pass  
 
-# def test_softmax(self):
-#     assrt([(3), (3)], lambda x: torch.nn.functional.softmax(x, dim=1), lambda x: picograd.nn.functional.softmax(x, 1))
+  # TODO:
+  def test_softmax(self):
+    helper_test_op([(45,65)], torch.nn.Softmax(dim=1), Tensor.softmax, atol=1e-7, grad_atol=1e-7)
+    helper_test_op([(45)], torch.nn.Softmax(dim=0), Tensor.softmax, atol=1e-7, grad_atol=1e-7)
+    helper_test_op([()], torch.nn.Softmax(dim=0), Tensor.softmax, atol=1e-7, grad_atol=1e-7)
+    helper_test_op([()], torch.nn.Softmax(dim=-1), Tensor.softmax, atol=1e-7, grad_atol=1e-7)
+  def test_softmax_other_axis(self):
+    helper_test_op([(10,10,10)], lambda x: x.softmax(0), atol=1e-7, grad_atol=2e-7)
+    helper_test_op([(10,10,10)], lambda x: x.softmax(1), atol=1e-7, grad_atol=2e-7)
+    helper_test_op([(10,10,10)], lambda x: x.softmax(2), atol=1e-7, grad_atol=2e-7)
+
+# SAMPLING TODO:
+# - softmax
+# - sum (for user debugging)
+# - multinomial. kolmgorov-smirnov.
+
+# TODO:
+# - https://en.wikipedia.org/wiki/Kolmogorov%E2%80%93Smirnov_test
+# - https://gist.github.com/devries/11405101
+def assrt_distribution(tiny_func, torch_func=None, numpy_func=None, shape=(40, 43), alpha=0.04):
+  assert not (torch_func is None and numpy_func is None), "no function to compare with"
+  Tensor.manual_seed(1337)
+  torch.manual_seed(1337)
+  np.random.seed(1337)
+
+  x1, x2 = tiny_func(*shape).numpy().flatten(), tiny_func(shape).numpy().flatten()
+  if numpy_func is not None: y = numpy_func(shape).flatten()
+  if torch_func is not None: z = torch_func(shape).numpy().flatten()
+
+  return (numpy_func is None or (kstest(x1, y) >= alpha and kstest(x2, y) >= alpha)) and \
+    (torch_func is None or (kstest(x1, z) >= alpha and kstest(x2, z) >= alpha))
+
+def ksprob(a):
+  fac, total, termbf = 2.0, 0.0, 0.0
+  a2 = -2.0 * a * a
+  for j in range(1, 101):
+    term = fac * math.exp(a2 * j * j)
+    total += term
+    if math.fabs(term) <= 0.001 * termbf or math.fabs(term) <= 1e-8 * total:
+      return total
+    fac = -fac
+    termbf = math.fabs(term)
+  return 1.0
+
+def kstest(l1, l2):
+  n1, n2 = len(l1), len(l2)
+  l1.sort()
+  l2.sort()
+  j1, j2, d, fn1, fn2 = 0, 0, 0.0, 0.0, 0.0
+  while j1 < n1 and j2 < n2:
+    d1, d2 = l1[j1], l2[j2]
+    if d1 <= d2:
+      fn1 = (float(j1) + 1.0) / float(n1)
+      j1 += 1
+    if d2 <= d1:
+      fn2 = (float(j2) + 1.0) / float(n2)
+      j2 += 1
+    dtemp = math.fabs(fn2 - fn1)
+    if dtemp > d:
+      d = dtemp
+  ne = float(n1 * n2) / float(n1 + n2)
+  nesq = math.sqrt(ne)
+  prob = ksprob((nesq + 0.12 + 0.11 / nesq) * d)
+  return prob
+
+def equal_distribution(tiny_func, torch_func=None, numpy_func=None, shape=(40, 43), alpha=0.04):
+  Tensor.manual_seed(1337)
+  torch.manual_seed(1337)
+  np.random.seed(1337)
+  assert not (torch_func is None and numpy_func is None), "no function to compare with"
+  x1 = tiny_func(*shape).numpy().flatten()
+  x2 = tiny_func(shape).numpy().flatten()
+  if numpy_func is not None: y = numpy_func(shape).flatten()
+  if torch_func is not None: z = torch_func(shape).numpy().flatten()
+  return (numpy_func is None or (kstest(x1, y) >= alpha and kstest(x2, y) >= alpha)) and \
+    (torch_func is None or (kstest(x1, z) >= alpha and kstest(x2, z) >= alpha))
+
+def normal_test(func, shape=(20, 23), alpha=0.05): return equal_distribution(func, numpy_func=lambda x: np.random.randn(*x), shape=shape, alpha=alpha)
+
+class TestRandom(unittest.TestCase):
+  def test_multinomial(self):
+    def _check_with_torch(w, num_samples, replacement):
+      tiny_res = Tensor(w).multinomial(num_samples, replacement=replacement)
+      torch_res = torch.tensor(w).multinomial(num_samples, replacement=replacement)
+      self.assertEqual(tiny_res.shape, torch_res.shape)
+      if torch_res.ndim == 1:
+        tiny_res = tiny_res.unsqueeze(0)
+        torch_res = torch_res.unsqueeze(0)
+      for i in range(torch_res.shape[0]):
+        self.assertTrue(equal_distribution(lambda *_: tiny_res[i], lambda _: torch_res[i]))
+
+    _check_with_torch(w=[0.231, 0., 1., 0.5], num_samples=2000, replacement=True)
+    _check_with_torch(w=[[0.2, 0.8]], num_samples=2000, replacement=True)  # 2D but only 1 row
+    _check_with_torch(w=[[0.453, 0., 1., 0.81], [0.1, 0.8, 0., 0.1]], num_samples=2000, replacement=True)  
+    # no-replacement isn't supported, unless taking only one sample
+  
+    # self.assertRaises(AssertionError, lambda: Tensor(w).multinomial(100, replacement=False))
+    # self.assertRaises(AssertionError, lambda: Tensor(2).multinomial(1, replacement=False))
+    # self.assertRaises(AssertionError, lambda: Tensor([1, 9]).multinomial(0, replacement=False))
 
 if __name__ == '__main__':
   np.random.seed(1337)
