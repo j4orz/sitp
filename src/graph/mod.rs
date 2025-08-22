62d2602d8b0926a5053af6d587a19354662404f2
jeffzh4ng <zhang.jeffreyd@gmail.com>
2025-08-22 08:55:04 -0400

    implement concrete bfs for al

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -1,165 +1,190 @@
-use std::{collections::{HashSet, VecDeque}, iter, hash::Hash, fmt::Debug};
-mod traversals; use traversals::IntoBfs;
-mod shortest_paths;
+mod mst;
+mod sp;
 
-pub trait VisitMap<NId> { // polymorphic over visitation storages (i.e dense/sparse representations)
-    fn visit(&mut self, v: &NId) -> bool;
-    fn unvisit(&mut self, v: &NId) -> bool;
-    fn visited(&self, v: NId) -> bool;
-}
+use std::collections::{HashSet, VecDeque};
 
-impl<NId, T> VisitMap<NId> for HashSet<T> { // TODO: HashSet<NId>?
-    fn visit(&mut self, v: &NId) -> bool { todo!() }
-    fn unvisit(&mut self, v: &NId) -> bool { todo!() }
-    fn visited(&self, v: NId) -> bool { todo!() }
-}
-// impl<NId, T> VisitMap<NId> for BitSet
+pub struct Graph { al: Vec<Vec<usize>> }
+impl Graph { pub fn new(al: Vec<Vec<usize>>) -> Self { Self { al, } } }
 
-pub trait Graph { // polymorphic over graph storage
-    type NId: Copy + PartialEq; // ids need to be copyable and identifiable
-    type EId: Copy + PartialEq;
-    fn node_ids(&self) -> impl Iterator<Item=Self::NId>;
-    fn neighbors(&self, v: Self::NId) -> impl Iterator<Item=Self::NId>;
-    type Map: VisitMap<Self::NId>;
-    fn visit_map(&self) -> Self::Map;
-    fn reset_map(&self, map: &mut Self::Map);
-    // _________________________________________________________________________
-
-    fn into_bfs(self, from: Self::NId) -> IntoBfs<Self> where Self: Sized {
-        let vm = self.visit_map(); // visit_map before self moves to IntoBfs combinator
-        IntoBfs { g: self, q: VecDeque::from([from]), visited: vm }
+impl Graph {
+    pub fn into_bfs(self, s: usize) -> IntoBfs {
+        IntoBfs { g: self, q: VecDeque::from([s]), v: HashSet::new(), }
     }
 }
 
-pub trait DirectedGraph : Graph {
-    fn succs(&self, v: Self::NId) -> impl Iterator<Item=Self::NId>;
-    fn preds(&self, v: Self::NId) -> impl Iterator<Item=Self::NId>;
+pub struct IntoBfs {
+    pub g: Graph,
+    pub q: VecDeque<usize>,
+    pub v: HashSet<usize>
 }
 
+impl Iterator for IntoBfs {
+    type Item = usize;
 
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(i) = self.q.pop_front() {
+            for j in &self.g.al[i] {
+                if !self.v.contains(&j) { self.q.push_back(j.clone()); }
+            }
 
-
-
-// ADJLL _______________________________________________________________________
-impl<N, E, I: Index> Graph for AdjLL<N, E, I> { // polymorphic over indices (edge devices)
-    type NId = NodeIndex<I>;
-    type EId = EdgeIndex<I>;
-    fn node_ids(&self) -> impl Iterator<Item=Self::NId> { iter::empty() }
-    fn neighbors(&self, v: Self::NId) -> impl Iterator<Item=Self::NId> { iter::empty() }
-    type Map = HashSet<Self::NId>;
-    fn visit_map(&self) -> Self::Map { todo!() }
-    fn reset_map(&self, map: &mut Self::Map) { todo!() }
-}
-
-pub trait Index: Copy + Default + Hash + Ord + Debug + 'static {
-    fn new(i: usize) -> Self;
-    fn index(&self) -> usize;
-    fn max() -> Self;
-}
-
-#[derive(Clone, Copy, PartialEq)] pub struct NodeIndex<I>(I);
-#[derive(Clone, Copy, PartialEq)] pub struct EdgeIndex<I>(I);
-impl<I> NodeIndex<I> { pub fn new(i: I) -> Self { NodeIndex(i)}}
-impl<I> EdgeIndex<I> { pub fn new(i: I) -> Self { EdgeIndex(i)}}
-
-// pub const INVALID_EDGE_INDEX: EdgeIndex = EdgeIndex(usize::MAX);
-// const OUTGOING: usize = 0;
-// const INCOMING: usize = 1;
-
-/// adjlist representation of a graph using linkedlists, based off rustc's linkedgraph,
-/// with some generic modifications (index and direction) based off the petgraph crate.
-/// `adjlinkedlist` contiguously stores the set of nodes and edges in two flat vectors,
-/// and each node stores it's adjacent neighbors with the head of a linkedlist of
-/// edge indices, which index into the vector of edges.
-/// see: https://smallcultfollowing.com/babysteps/blog/2015/04/06/modeling-graphs-in-rust-using-vector-indices/
-/// see: https://doc.rust-lang.org/nightly/nightly-rustc/rustc_data_structures/graph/linked_graph/struct.LinkedGraph.html
-/// 
-/// TODO: logical graph
-///       physical graph
-pub struct AdjLL<N, E, I> { nodes: Vec<AdjLLNode<N, I>>, edges: Vec<AdjLLEdge<E, I>> }
-struct AdjLLNode<N, I> { data: N, head_edges: [EdgeIndex<I>; 2] }
-struct AdjLLEdge<E, I> { data: E, next_edges: [EdgeIndex<I>; 2], src: NodeIndex<I>, tgt: NodeIndex<I> }
-
-
-
-impl<N, E, I: Index> AdjLL<N, E, I> {
-    pub fn new() -> Self { Self { nodes: Vec::new(), edges: Vec::new() }}
-    pub fn from_edges<It: IntoIterator>(i: It) -> Self { todo!() }
-
-    pub fn add_node(&mut self, data: N) -> NodeIndex<I> {
-        // let i = NodeIndex(self.nodes.len());
-        // self.nodes.push(Node { data, head_edges: [INVALID_EDGE_INDEX, INVALID_EDGE_INDEX] });
-        // i
-        todo!()
-    }
-    pub fn add_edge(&mut self, data: E, src: NodeIndex<I>, tgt: NodeIndex<I>) -> EdgeIndex<I> {
-        // let src_outgoing_head = self.nodes[src.0].head_edges[OUTGOING];
-        // let tgt_incoming_head = self.nodes[tgt.0].head_edges[INCOMING];
-        // self.edges.push(Edge { data, next_edges: todo!(), src, tgt });
-        todo!()
-    }
-
-    pub fn node_weights(&self) -> impl Iterator<Item=N> { std::iter::empty() }
-    pub fn node_references(&self) -> impl Iterator<Item=(NodeIndex<I>, N)> { std::iter::empty() }
-    // pub fn update_node() -> () {}
-    // pub fn update_edge() -> () {}
-
-    // pub fn delete_node() -> () {}
-    // pub fn delete_edge() -> () {}
-}
-
-mod test_adjll {
-    #[test]
-    fn foo() {
+            return Some(i);
+        }
         
+        None
     }
 }
-// _____________________________________________________________________________
+
+// //!            space            read                     update            delete
+// //! adjlist
+// //! adjmat
+// //! csr
+// use std::{collections::{HashSet, VecDeque}, fmt::Debug, hash::Hash, iter, marker::PhantomData};
+// mod traversals; use traversals::IntoBfs;
+// mod shortest_paths;
+
+// pub trait Graph { // polymorphic over graph storage
+//     type NId: Copy + PartialEq; // ids need to be copyable and identifiable
+//     type EId: Copy + PartialEq;
+//     fn node_ids(&self) -> impl Iterator<Item=Self::NId>;
+//     fn neighbors(&self, v: Self::NId) -> impl Iterator<Item=Self::NId>;
+//     type Map: VisitMap<Self::NId>;
+//     fn visit_map(&self) -> Self::Map;
+//     fn reset_map(&self, map: &mut Self::Map);
+//     // _________________________________________________________________________
+
+//     fn into_bfs(self, from: Self::NId) -> IntoBfs<Self> where Self: Sized {
+//         let vm = self.visit_map(); // visit_map before self moves to IntoBfs combinator
+//         IntoBfs { g: self, q: VecDeque::from([from]), visited: vm }
+//     }
+// }
+
+// pub trait DirectedGraph : Graph {
+//     fn succs(&self, v: Self::NId) -> impl Iterator<Item=Self::NId>;
+//     fn preds(&self, v: Self::NId) -> impl Iterator<Item=Self::NId>;
+// }
+
+// //         time
 
 
-impl Graph for AdjMat {
-    type NId;
-    type EId;
-
-    fn node_ids(&self) -> impl Iterator<Item=Self::NId> { todo!() }
-    fn neighbors(&self, v: Self::NId) -> impl Iterator<Item=Self::NId> { todo!() }
-
-    type Map;
-    fn visit_map(&self) -> Self::Map { todo!() }
-    fn reset_map(&self, map: &mut Self::Map) { todo!() }
-}
-pub struct AdjMat {}
-impl AdjMat {}
-
-pub struct AdjHashMap<N, E, Idx> { nodes: Vec<AdjLLNode<N, Idx>>, edges: Vec<AdjLLEdge<E, Idx>> }
-impl<N, E, Idx> AdjHashMap<N, E, Idx> {
-    pub fn new() -> Self { Self { nodes: Vec::new(), edges: Vec::new() }}
-    pub fn from_edges<I: IntoIterator>(i: I) -> Self { todo!() }
-    pub fn foobazz() -> () { todo!() }
-
-    // pub fn add_node(&mut self, data: N) -> NodeIndex<Idx> {
-    //     let i = NodeIndex(self.nodes.len());
-    //     self.nodes.push(Node { data, head_edges: [INVALID_EDGE_INDEX, INVALID_EDGE_INDEX] });
-    //     i
-    // }
-
-    // pub fn add_edge(&mut self, data: E, src: NodeIndex<Idx>, tgt: NodeIndex<Idx>) -> EdgeIndex<Idx> {
-    //     let src_outgoing_head = self.nodes[src.0].head_edges[OUTGOING];
-    //     let tgt_incoming_head = self.nodes[tgt.0].head_edges[INCOMING];
-    //     self.edges.push(Edge { data, next_edges: todo!(), src, tgt });
-    //     todo!()
-    // }
-
-    // pub fn update_node() -> () {}
-    // pub fn update_edge() -> () {}
-
-    // pub fn delete_node() -> () {}
-    // pub fn delete_edge() -> () {}
-}
 
 
-// TODO:
-// - unsafe for bfsmut iterator adapter??
-// - parallel bfs
-// size, alignment of Option<usize> is ... NPO...
-// TODO: Nethercote. measure.
+// //                                  ^^^^^^^^^^
+// // =================================ALGORITHMS==================================
+
+
+
+
+
+
+// pub trait VisitMap<NId> { // polymorphic over visitation storages (i.e dense/sparse NIds)
+//     fn visit(&mut self, v: &NId) -> bool;
+//     fn unvisit(&mut self, v: &NId) -> bool;
+//     fn visited(&self, v: NId) -> bool;
+// }
+
+// impl<NId, T> VisitMap<NId> for HashSet<T> { // TODO: HashSet<NId>?
+//     fn visit(&mut self, v: &NId) -> bool { todo!() }
+//     fn unvisit(&mut self, v: &NId) -> bool { todo!() }
+//     fn visited(&self, v: NId) -> bool { todo!() }
+// }
+// // impl<NId, T> VisitMap<NId> for BitSet
+
+
+
+
+
+
+
+
+
+
+// // ===============================DATA STRUCTURES===============================
+// //                                vvvvvvvvvvvvvvv
+
+// // ADJLL _______________________________________________________________________
+// impl<N, E, I: Index, Ty> Graph for AdjList<N, E, I, Ty> { // polymorphic over indices (edge devices)
+//     type NId = NodeIndex<I>;
+//     type EId = EdgeIndex<I>;
+//     fn node_ids(&self) -> impl Iterator<Item=Self::NId> { iter::empty() }
+//     fn neighbors(&self, v: Self::NId) -> impl Iterator<Item=Self::NId> { iter::empty() }
+//     type Map = HashSet<Self::NId>;
+//     fn visit_map(&self) -> Self::Map { todo!() }
+//     fn reset_map(&self, map: &mut Self::Map) { todo!() }
+// }
+
+// pub trait Index: Copy + Default + Hash + Ord + Debug + 'static {
+//     fn new(i: usize) -> Self;
+//     fn index(&self) -> usize;
+//     fn max() -> Self;
+// }
+
+// #[derive(Clone, Copy, PartialEq)] pub struct NodeIndex<I>(I);
+// #[derive(Clone, Copy, PartialEq)] pub struct EdgeIndex<I>(I);
+// impl<I> NodeIndex<I> { pub fn new(i: I) -> Self { NodeIndex(i)}}
+// impl<I> EdgeIndex<I> { pub fn new(i: I) -> Self { EdgeIndex(i)}}
+
+// // pub const INVALID_EDGE_INDEX: EdgeIndex = EdgeIndex(usize::MAX);
+// // const OUTGOING: usize = 0;
+// // const INCOMING: usize = 1;
+// struct AdjLLNode<N, I> { data: N, head_edges: [EdgeIndex<I>; 2] }
+// struct AdjLLEdge<E, I> { data: E, next_edges: [EdgeIndex<I>; 2], src: NodeIndex<I>, tgt: NodeIndex<I> }
+// pub struct AdjList<N, E, I, Ty> {
+//     nodes: Vec<AdjLLNode<N, I>>,
+//     edges: Vec<AdjLLEdge<E, I>>,
+//     ty: PhantomData<Ty>
+// }
+// impl<N, E, I: Index, Ty> AdjList<N, E, I, Ty> {
+//     pub fn new() -> Self { Self { nodes: Vec::new(), edges: Vec::new() }}
+//     pub fn from_outgoing_edges<It: IntoIterator>(i: It) -> Self { todo!() }
+//     pub fn node_weights(&self) -> impl Iterator<Item=N> { std::iter::empty() }
+//     pub fn node_references(&self) -> impl Iterator<Item=(NodeIndex<I>, N)> { std::iter::empty() }
+// }
+
+// mod test_adjll {
+//     #[test]
+//     fn foo() {
+        
+//     }
+// }
+
+// // STABLEADJLL _________________________________________________________________
+
+
+
+
+// // ADJVEC ______________________________________________________________________
+// struct Edge<E> {
+//     node: usize,
+//     weight: E
+// }
+
+
+// pub struct AdjVec<N, E, I, Ty> { al: Vec<Vec<Edge<E>>> }
+// impl<N, E, I: Index, Ty> AdjVec<N, E, I, Ty> {
+//     pub fn new() -> Self { Self { al: vec![vec![]] }}
+//     pub fn from_edges<It: IntoIterator>(i: It) -> Self { todo!() }
+//     pub fn node_weights(&self) -> impl Iterator<Item=N> { std::iter::empty() }
+//     pub fn node_references(&self) -> impl Iterator<Item=(NodeIndex<I>, N)> { std::iter::empty() }
+// }
+
+
+
+
+
+
+
+
+// // ADJMAT ______________________________________________________________________
+// pub struct AdjMat<N, E, Ty> {
+//     mat: Vec<Vec<Option<T>>>
+// }
+// impl<N, E, Ty> AdjMat<N, E, Ty> {
+//     fn new() -> Self { Self { mat: vec![vec![]] }}
+// }
+
+
+// // TODO:
+// // - unsafe for bfsmut iterator adapter??
+// // - parallel bfs
+// // size, alignment of Option<usize> is ... NPO...
+// // TODO: Nethercote. measure.
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
-use elements::graph::{AdjLL, Graph};
+use elements::graph::Graph;
 
 fn main() {
     println!("Hello, world!");
-    let g = AdjLL::from_edges(&[
-        (0, 1), (0, 2), (0, 3),
-        (1, 2), (1, 3),
-        (2, 3),
+    let g = Graph::new(vec![
+        vec![1, 2, 7, 8],
+        vec![3, 4],
+        vec![5, 6],
+        vec![], // 3
+        vec![], // 4
+        vec![], // 5
+        vec![], // 6
+        vec![], // 7
+        vec![], // 8
     ]);
-    let foo = g
-        .into_bfs(NodeIndex::new(2))
-        .map(|nid| nid+2).collect::<Vec<_>>();
+
+    for i in g.into_bfs(0) { println!("{:?}", i); }
+
+
+    // let g = AdjList::from_outgoing_edges(&[
+    //     (0, 1), (0, 2), (0, 3),
+    //     (1, 2), (1, 3),
+    //     (2, 3),
+    // ]);
+    // let foo = g
+    //     .into_bfs(NodeIndex::new(2))
+    //     .map(|nid| nid+2).collect::<Vec<_>>();
 }
