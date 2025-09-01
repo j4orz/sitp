bee3e8333762d55355e73542264630ab589cfd25
jeffzh4ng <zhang.jeffreyd@gmail.com>
2025-09-01 13:03:57 -0400

    move src to stdnot

---
+++ stdnot/src/main.rs
@@ -0,0 +1,46 @@
+// use elements::graph::Graph;
+// use elements::iterator::List;
+
+fn main() {
+    todo!()
+    // println!("Hello, world!");
+    // let mut l = List::new();
+    // l.push(1); l.push(2); l.push(3);
+
+    // let mut l2 = List::map(&l, |x| {
+    //     x * 2
+    // });
+
+    // println!("{:?}", l.pop());
+    // println!("{:?}", l.pop());
+    // println!("{:?}", l.pop());
+
+    // println!("{:?}", l2.pop());
+    // println!("{:?}", l2.pop());
+    // println!("{:?}", l2.pop());
+
+
+    // let g = Graph::new(vec![
+    //     vec![1, 2, 7, 8],
+    //     vec![3, 4],
+    //     vec![5, 6],
+    //     vec![], // 3
+    //     vec![], // 4
+    //     vec![], // 5
+    //     vec![], // 6
+    //     vec![], // 7
+    //     vec![], // 8
+    // ]);
+
+    // for i in g.into_bfs(0) { println!("{:?}", i); }
+
+
+    // let g = AdjList::from_outgoing_edges(&[
+    //     (0, 1), (0, 2), (0, 3),
+    //     (1, 2), (1, 3),
+    //     (2, 3),
+    // ]);
+    // let foo = g
+    //     .into_bfs(NodeIndex::new(2))
+    //     .map(|nid| nid+2).collect::<Vec<_>>();
+}
